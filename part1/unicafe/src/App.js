c0ac894dc3fc831b9e14448d84a969e0dba97386
leprecon73 <leprecon73@gmail.com>
2022-09-18 03:38:52 +0400

    1.11

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -2,7 +2,7 @@ import { useState } from 'react'
 
 const StatisticLine = (props) => {
   return (
-    <div>{props.text} {props.value}</div>
+    <tr><td>{props.text}</td><td>{props.value}</td></tr>
   )
 }
 
@@ -15,18 +15,20 @@ const Statistics = (props) => {
         <div>
           No feedback given
         </div>
-        
       )
     } 
 
     return (
       <div>
-        <StatisticLine text="good" value ={good} />
-        <StatisticLine text="neutral" value ={neutral} />
-        <StatisticLine text="bad" value ={bad} />
-        <div>all {bad+good+neutral}</div>
-        <div>average {(bad * (-1) + good)/(bad+good+neutral)}</div>
-        <div>positive {(good/(bad+good+neutral)) * 100} %</div>
+        <table>
+          <StatisticLine text="good" value ={good} />
+          <StatisticLine text="neutral" value ={neutral} />
+          <StatisticLine text="bad" value ={bad} />
+          
+          <tr><td>all</td><td>{bad+good+neutral}</td></tr>
+          <tr><td>average</td><td>{(bad * (-1) + good)/(bad+good+neutral)}</td></tr>
+          <tr><td>positive</td><td>{(good/(bad+good+neutral)) * 100} %</td></tr>
+        </table>
       </div>
     )
   }

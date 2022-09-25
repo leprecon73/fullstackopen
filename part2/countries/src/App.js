02cd9a7e3e7406c3559075bd72c8e45769aa7607
leprecon73 <leprecon73@gmail.com>
2022-09-26 02:38:18 +0400

    2.13*: Data for countries, step2 (reThink, reLearn, reFactor)

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,27 +1,6 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
-
-const Country = ({ countries, filter, setOneCountry }) => {
-
-    const filteredCountries = countries.map(e => e.name.common)
-          .filter(e => e.toLowerCase().includes(filter.toLowerCase()))
-          .sort()
-    
-    if (filteredCountries.length === 1) {
-      setOneCountry(filteredCountries[0])      
-    } else if (filteredCountries.length > 10) {
-      setOneCountry('')
-      return (
-        <div>Too many matches, specify another filter<br /></div>
-      )
-    } else {
-      return (
-        <div>{filteredCountries.map((e, i) => <div key={i}>{e}<button onClick={() => setOneCountry(e)}>show</button></div>)}</div>
-      )
-    }
-}
-
 const Filter = ({filter, handleFilter}) => {
   return (
     <form >
@@ -35,32 +14,42 @@ const Filter = ({filter, handleFilter}) => {
   )
 }
 
-const OneCountry = ({oneCountry, countries}) => {
-  if (oneCountry.length > 0) {
-
-    const country = countries[countries.findIndex(e => e.name.common === oneCountry)]
-
+const Country = ({filteredCountries, filter, setFilteredCountries}) => {
+  if (filteredCountries.length > 1 && filteredCountries.length <= 10) {
     return (
-    <div>
-      <br /><h1>{oneCountry}</h1><br />
-      capital {country.capital}<br />
-      area {country.area}<br />
-      <h3>languages:</h3>
-      <ul>
-      {Object.values(country.languages).map((e, i) => <li key={i}>{e}</li>)}<br />
-      </ul>
-      
-      <img src={country.flags.png} height={'10%'} width={'10%'}></img>
-    </div>
+      <div>{filteredCountries.map((e, i) => <div key={i}>{e}<button onClick={() => setFilteredCountries([e])}>show</button></div>)}</div>
+    )
+  } else if (filteredCountries.length > 10 && filter !== '') {
+    return (
+      <div>Too many matches, specify another filter<br /></div>
+    )
+  }
+}
+
+const OneCountry = ({filteredCountries, countries}) => {
+  if (filteredCountries.length === 1) {
+    const country = countries[countries.findIndex(e => e.name.common === filteredCountries[0])]
+    return (
+      <div>
+        <br /><h1>{country.name.common}</h1><br />
+        capital {country.capital}<br />
+        area {country.area}<br />
+        <h3>languages:</h3>
+        <ul>
+        {Object.values(country.languages).map((e, i) => <li key={i}>{e}</li>)}<br />
+        </ul>
+        
+        <img src={country.flags.png} height={'10%'} width={'10%'}></img>
+      </div>
     )
   }
 }
 
 const App = () => {
-  const [countries, setCountries] = useState([]) 
+  const [countries, setCountries] = useState([])
   const [filter, setFilter] = useState('')
-  const [oneCountry, setOneCountry] = useState('') 
-  
+  const [filteredCountries, setFilteredCountries] = useState([])
+    
   useEffect(() => {
     axios
       .get('https://restcountries.com/v3.1/all')
@@ -68,19 +57,26 @@ const App = () => {
         setCountries(response.data)
       })
   }, [])
-
+  
   const handleFilter = (event) => {
     setFilter(event.target.value)
-  }
 
-  
+    if (event.target.value === '') { 
+      const fCountries = [...filteredCountries]
+      setFilteredCountries(fCountries)
+      } else {
+      const fCountries = countries.map(e => e.name.common)
+      .filter(e => e.toLowerCase().includes(event.target.value.toLowerCase()))
+      .sort()
+      setFilteredCountries(fCountries)
+    }
+  }
+    
   return ( 
     <div> 
-      <Filter filter={filter} handleFilter={handleFilter}/>
-
-      <Country countries={countries} filter={filter} setOneCountry={setOneCountry}/>
-
-      <OneCountry oneCountry={oneCountry} countries={countries}/>
+      <Filter countries={countries} filter={filter} handleFilter={handleFilter} filteredCountries={filteredCountries}/>
+      <Country filteredCountries={filteredCountries} filter={filter} setFilteredCountries={setFilteredCountries}/>
+      <OneCountry filteredCountries={filteredCountries} countries={countries}/>
     </div>
   )
 }
